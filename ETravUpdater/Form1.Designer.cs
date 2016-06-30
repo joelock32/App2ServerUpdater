9507fbac4338cfa0bf51285eadf5e4934cd84598
lockhj <joseph.lockhart@astrodynetdi.com>
2016-06-30 15:56:45 -0400

    initial

--- ETravUpdater/Form1.Designer.cs
+++ ETravUpdater/Form1.Designer.cs
@@ -28,21 +28,208 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
+            this.checkButton1 = new DevExpress.XtraEditors.CheckButton();
+            this.checkButton2 = new DevExpress.XtraEditors.CheckButton();
+            this.checkButton3 = new DevExpress.XtraEditors.CheckButton();
+            this.checkButton4 = new DevExpress.XtraEditors.CheckButton();
+            this.checkButton5 = new DevExpress.XtraEditors.CheckButton();
+            this.checkButton6 = new DevExpress.XtraEditors.CheckButton();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.dropDownButton1 = new DevExpress.XtraEditors.DropDownButton();
+            this.dropDownButton2 = new DevExpress.XtraEditors.DropDownButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panelControl3 = new DevExpress.XtraEditors.PanelControl();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
+            this.panelControl2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl3)).BeginInit();
+            this.panelControl3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.simpleButton2);
+            this.panelControl1.Controls.Add(this.simpleButton1);
+            this.panelControl1.Controls.Add(this.checkButton6);
+            this.panelControl1.Controls.Add(this.checkButton5);
+            this.panelControl1.Controls.Add(this.checkButton4);
+            this.panelControl1.Controls.Add(this.checkButton3);
+            this.panelControl1.Controls.Add(this.checkButton2);
+            this.panelControl1.Controls.Add(this.checkButton1);
+            this.panelControl1.Location = new System.Drawing.Point(3, 48);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(269, 260);
+            this.panelControl1.TabIndex = 0;
+            // 
+            // panelControl2
+            // 
+            this.panelControl2.Controls.Add(this.dropDownButton2);
+            this.panelControl2.Controls.Add(this.dropDownButton1);
+            this.panelControl2.Location = new System.Drawing.Point(297, 60);
+            this.panelControl2.Name = "panelControl2";
+            this.panelControl2.Size = new System.Drawing.Size(426, 121);
+            this.panelControl2.TabIndex = 1;
+            // 
+            // checkButton1
+            // 
+            this.checkButton1.Location = new System.Drawing.Point(6, 29);
+            this.checkButton1.Name = "checkButton1";
+            this.checkButton1.Size = new System.Drawing.Size(47, 25);
+            this.checkButton1.TabIndex = 0;
+            this.checkButton1.Text = "*.EXE";
+            // 
+            // checkButton2
+            // 
+            this.checkButton2.Location = new System.Drawing.Point(6, 60);
+            this.checkButton2.Name = "checkButton2";
+            this.checkButton2.Size = new System.Drawing.Size(47, 25);
+            this.checkButton2.TabIndex = 1;
+            this.checkButton2.Text = "*.DLL";
+            // 
+            // checkButton3
+            // 
+            this.checkButton3.Location = new System.Drawing.Point(6, 92);
+            this.checkButton3.Name = "checkButton3";
+            this.checkButton3.Size = new System.Drawing.Size(47, 25);
+            this.checkButton3.TabIndex = 2;
+            this.checkButton3.Text = "*.FRF";
+            // 
+            // checkButton4
+            // 
+            this.checkButton4.Location = new System.Drawing.Point(6, 123);
+            this.checkButton4.Name = "checkButton4";
+            this.checkButton4.Size = new System.Drawing.Size(47, 25);
+            this.checkButton4.TabIndex = 3;
+            this.checkButton4.Text = "*.FR3";
+            // 
+            // checkButton5
+            // 
+            this.checkButton5.Location = new System.Drawing.Point(6, 154);
+            this.checkButton5.Name = "checkButton5";
+            this.checkButton5.Size = new System.Drawing.Size(47, 25);
+            this.checkButton5.TabIndex = 4;
+            this.checkButton5.Text = "*.INI";
+            // 
+            // checkButton6
+            // 
+            this.checkButton6.Location = new System.Drawing.Point(6, 185);
+            this.checkButton6.Name = "checkButton6";
+            this.checkButton6.Size = new System.Drawing.Size(47, 25);
+            this.checkButton6.TabIndex = 5;
+            this.checkButton6.Text = "*.XML";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(6, 0);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(115, 28);
+            this.simpleButton1.TabIndex = 6;
+            this.simpleButton1.Text = "Insert Application";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(128, 0);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(135, 28);
+            this.simpleButton2.TabIndex = 7;
+            this.simpleButton2.Text = "Reinsert Last App.";
+            // 
+            // dropDownButton1
+            // 
+            this.dropDownButton1.Location = new System.Drawing.Point(5, 6);
+            this.dropDownButton1.Name = "dropDownButton1";
+            this.dropDownButton1.Size = new System.Drawing.Size(343, 23);
+            this.dropDownButton1.TabIndex = 0;
+            this.dropDownButton1.Text = "Location Selection\r\nUS\r\nChina";
+            // 
+            // dropDownButton2
+            // 
+            this.dropDownButton2.Location = new System.Drawing.Point(5, 48);
+            this.dropDownButton2.Name = "dropDownButton2";
+            this.dropDownButton2.Size = new System.Drawing.Size(343, 22);
+            this.dropDownButton2.TabIndex = 1;
+            this.dropDownButton2.Text = "Destination Location";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // panelControl3
+            // 
+            this.panelControl3.Controls.Add(this.gridControl1);
+            this.panelControl3.Location = new System.Drawing.Point(3, 314);
+            this.panelControl3.Name = "panelControl3";
+            this.panelControl3.Size = new System.Drawing.Size(714, 239);
+            this.panelControl3.TabIndex = 2;
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Location = new System.Drawing.Point(0, 5);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(467, 219);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.Appearance.Options.UseFont = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 278);
+            this.ClientSize = new System.Drawing.Size(737, 550);
+            this.Controls.Add(this.panelControl3);
+            this.Controls.Add(this.panelControl2);
+            this.Controls.Add(this.panelControl1);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Insert Application Updater";
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).EndInit();
+            this.panelControl2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl3)).EndInit();
+            this.panelControl3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.CheckButton checkButton6;
+        private DevExpress.XtraEditors.CheckButton checkButton5;
+        private DevExpress.XtraEditors.CheckButton checkButton4;
+        private DevExpress.XtraEditors.CheckButton checkButton3;
+        private DevExpress.XtraEditors.CheckButton checkButton2;
+        private DevExpress.XtraEditors.CheckButton checkButton1;
+        private DevExpress.XtraEditors.PanelControl panelControl2;
+        private DevExpress.XtraEditors.DropDownButton dropDownButton2;
+        private DevExpress.XtraEditors.DropDownButton dropDownButton1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private DevExpress.XtraEditors.PanelControl panelControl3;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
     }
 }
 
--- ETravUpdater/Form1.cs
+++ ETravUpdater/Form1.cs
@@ -3,17 +3,136 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace ETravUpdater
 {
     public partial class Form1 : DevExpress.XtraEditors.XtraForm
     {
+        string xfile;
+        string fn;
+        string[] versioninfo;
+        string FileNameof;
+        String ConnStr = "Data Source=etrav-hack;Initial Catalog=Images;Persist Security Info=True;User ID=Application;Password=noitacilppa";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                xfile = openFileDialog1.FileName;
+                FileNameof = Path.GetFileName(xfile);
+                fn = Path.GetFileNameWithoutExtension(xfile);
+                versioninfo = fn.Split('_');
+                Version v;
+                if (versioninfo.Length > 1 && Version.TryParse(versioninfo.Last(), out v))
+                {
+                    Console.Write("Major:{0}, Minor:{1}", v.Major, v.Minor);
+                }
+                databaseFilePut(xfile);
+            }
+            Console.WriteLine(result); // <-- For debugging use.
+        }
+
+        public  void databaseFilePut(string varFilePath)
+        {
+            byte[] file;
+            using (var stream = new FileStream(varFilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = new BinaryReader(stream))
+                {
+                    file = reader.ReadBytes((int)stream.Length);
+                }
+            }
+            using (SqlConnection SqlConn = new SqlConnection(ConnStr))
+            using (SqlCommand command = SqlConn.CreateCommand())
+            {
+                command.CommandText = "UPDATE tblapplications SET Application=@binapp, SET Version=@versioninfo FileDate=getdate() ";
+                command.Parameters.AddWithValue("@binapp", file);
+                command.Parameters.AddWithValue("@versioninfo", versioninfo);
+
+                SqlConn.Open();
+
+                command.ExecuteNonQuery();
+                
+                SqlConn.Close();
+            }
+        }
+
+        public void databaseFileRead(string varID, string varPathToNewLocation)
+        {
+            using (SqlConnection SqlConn = new SqlConnection(ConnStr))
+            using (SqlCommand command = SqlConn.CreateCommand())
+            {
+                command.CommandText = @"SELECT [Application] FROM tblapplications WHERE [RaportID] = @varID";
+                command.Parameters.AddWithValue("@varID", varID);
+                using (var sqlQueryResult = command.ExecuteReader())
+                    if (sqlQueryResult != null)
+                    {
+                        sqlQueryResult.Read();
+                        var blob = new Byte[(sqlQueryResult.GetBytes(0, 0, null, 0, int.MaxValue))];
+                        sqlQueryResult.GetBytes(0, 0, blob, 0, blob.Length);
+                        using (var fs = new FileStream(varPathToNewLocation, FileMode.Create, FileAccess.Write))
+                            fs.Write(blob, 0, blob.Length);
+                    }
+            }
+        }
+
+        public MemoryStream databaseFileRead(string varID)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            using (SqlConnection SqlConn = new SqlConnection(ConnStr))
+            using (SqlCommand command = SqlConn.CreateCommand())
+            {
+                command.CommandText = @"SELECT [Application] FROM tblapplications WHERE [RaportID] = @varID";
+                command.Parameters.AddWithValue("@varID", varID);
+                using (var sqlQueryResult = command.ExecuteReader())
+                    if (sqlQueryResult != null)
+                    {
+                        sqlQueryResult.Read();
+                        var blob = new Byte[(sqlQueryResult.GetBytes(0, 0, null, 0, int.MaxValue))];
+                        sqlQueryResult.GetBytes(0, 0, blob, 0, blob.Length);
+                        //using (var fs = new MemoryStream(memoryStream, FileMode.Create, FileAccess.Write)) {
+                        memoryStream.Write(blob, 0, blob.Length);
+                        //}
+                    }
+            }
+            return memoryStream;
+        }
+
+        public int databaseFilePut(MemoryStream fileToPut)
+        {
+            int varID = 0;
+            byte[] file = fileToPut.ToArray();
+            const string preparedCommand = @"
+                    INSERT INTO tblapplications
+                               ([RaportPlik])
+                         VALUES
+                               (@File)
+                        SELECT [RaportID] FROM [dbo].[Raporty]
+            WHERE [RaportID] = SCOPE_IDENTITY()
+                    ";
+            using (SqlConnection SqlConn = new SqlConnection(ConnStr))
+            using (SqlCommand command = SqlConn.CreateCommand())
+            {
+                command.Parameters.Add("@File", SqlDbType.VarBinary, file.Length).Value = file;
+
+                using (var sqlWriteQuery = command.ExecuteReader())
+                    while (sqlWriteQuery != null && sqlWriteQuery.Read())
+                    {
+                        varID = sqlWriteQuery["RaportID"] is int ? (int)sqlWriteQuery["RaportID"] : 0;
+                    }
+            }
+            return varID;
         }
     }
 }
